bde4e2ff0676df8bba0172993c3d6ad176e44da0
Alexandre Mello <alexandre.rmello@gmail.com>
2014-02-04 18:21:36 -0200

    add follow, and followed posts

--- app/views.py
+++ app/views.py
@@ -1,37 +1,26 @@
 from flask import render_template, flash, redirect, session, url_for, request, g
 from flask.ext.login import login_user, logout_user, current_user, login_required
 from app import app, db, lm, oid
-from forms import LoginForm, EditForm
-from models import User, ROLE_USER, ROLE_ADMIN
+from forms import LoginForm, EditForm, PostForm
+from models import User, ROLE_USER, ROLE_ADMIN, Post
 from datetime import datetime
 
-@app.route('/')
-@app.route('/index')
+@app.route('/', methods = ['GET', 'POST'])
+@app.route('/index', methods = ['GET', 'POST'])
 @login_required
 def index():
-	user  = g.user
-	posts = [ # fake array of posts 
-		{
-			'author': { 'nickname': 'John' },
-			'body': 'Beautiful day in Portland!'
-		},
-		{
-			'author': { 'nickname': 'Susan' },
-			'body': 'The Avengers movie was so cool!'
-		},
-		{
-			'author': { 'nickname': 'Keith' },
-			'body': 'Have a nice day!'
-		},
-		{
-			'author': { 'nickname': 'Daniel' },
-			'body': 'no such a word...'
-		}
-	]
+	form = PostForm()
+	if form.validate_on_submit():
+		post = Post(body = form.post.data, timestamp = datetime.utcnow(), author = g.user)
+		db.session.add(post)
+		db.session.commit()
+		flash('Your post is now live!')
+		return redirect(url_for('index'))
+	posts = g.user.followed_posts().all()
 
 	return render_template("index.html", 
 		title = 'Home', 
-		user  = user,
+		form  = form,
 		posts = posts)
 
 @app.route('/user/<nickname>')
@@ -66,6 +55,44 @@ def edit():
 	return render_template('edit.html',
 		form = form)
 
+@app.route('/follow/<nickname>')
+@login_required
+def follow(nickname):
+	user = User.query.filter_by(nickname = nickname).first()
+	if user == None:
+		flash('User ' + nickname + ' not found.')
+		return redirect(url_for('index'))
+	if user == g.user:
+		flash('You can\'t follow yourself!')
+		return redirect(url_for('user', nickname = nickname)) 
+	u = g.user.follow(user)
+	if u is None:
+		flash('Cannot follow ' + nickname + '.')
+		return redirect(url_for('user', nickname = nickname))
+	db.session.add(u)
+	db.session.commit()
+	flash('You are now following ' + nickname + '!')
+	return redirect(url_for('user', nickname = nickname))
+
+@app.route('/unfollow/<nickname>')
+@login_required
+def unfollow(nickname):
+	user = User.query.filter_by(nickname = nickname).first()
+	if user == None:
+		flash('User ' + nickname + ' not found.')
+		return redirect(url_for('index'))
+	if user == g.user:
+		flash('You can\'t unfollow yourself!')
+		return redirect(url_for('user', nickname = nickname))
+	u = g.user.unfollow(user)
+	if u is None: 
+		flash('Cannot unfollow ' + nickname + '.')
+		return redirect(url_for('user', nickname = nickname))
+	db.session.add(u)
+	db.session.commit()
+	flash('You have stopped following ' + nickname + '.')
+	return redirect(url_for('user', nickname = nickname))
+
 @app.route('/login', methods = ['GET', 'POST'])
 @oid.loginhandler
 def login():
@@ -98,6 +125,9 @@ def after_login(resp):
 		nickname = User.make_unique_nickname(nickname)
 		user = User(nickname = nickname, email = resp.email, role = ROLE_USER)
 		db.session.add(user)
+		db.session.commit()
+		# make the user follow him/herself
+		db.session.add(user.follow(user))
 		db.session.commit()
 	remember_me = False
 	if 'remember_me' in session:
